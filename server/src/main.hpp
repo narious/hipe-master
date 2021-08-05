d52b3ada5674c5a9bdd536f1d0f575c145c7eef0
Di Liu <liudi12631@gmail.com>
2021-08-05 12:48:29 +1000

    comment and format `server/main` files.

--- server/src/main.hpp
+++ server/src/main.hpp
@@ -18,41 +18,46 @@
 
 #pragma once
 
-//DECLARATIONS FOR GLOBAL RESOURCES
+// DECLARATIONS FOR GLOBAL RESOURCES
 
 class Connection;
 
+// string-format of UID number of the Unix user that owns this hiped instance.
 extern std::string uid;
-//string-format of UID number of the Unix user that owns this hiped instance.
 
+// random number device path (e.g. /dev/random). If empty, default behaviour is
+// used.
 extern std::string randomDevice;
-//random number device path (e.g. /dev/random). If empty, default behaviour is used.
 
+// verbose mode (unless overridden by --silent arg) means success messages are
+// printed to stderr.
 extern bool verbose;
-//verbose mode (unless overridden by --silent arg) means success messages are
-//printed to stderr.
 
+// as specified by --fill argument. When true, top-level windows are
+// automatically resized to fill the entire screen. This is useful when running
+// in a bare X session without a window manager.
 extern bool fillscreen;
-//as specified by --fill argument. When true, top-level windows are automatically
-//resized to fill the entire screen. This is useful when running in a bare X
-//session without a window manager.
 
+// the socket descriptor in which we listen for new client connection requests.
 extern int serverFD;
-//the socket descriptor in which we listen for new client connection requests.
 
-//self-registration functions for connections to call when they are created/destructed.
-void registerConnection(Connection*, int fd);
-void deregisterConnection(Connection*);
+// self-registration functions for connections to call when they are
+// created/destructed.
+void registerConnection(Connection *, int fd);
+void deregisterConnection(Connection *);
 bool serviceConnections();
 
-extern KeyList* topLevelKeyList;
-extern std::string keyFilePath; //path and filename to store next available top-level key in.
+extern KeyList *topLevelKeyList;
+// path and filename to store next available top-level key in.
+extern std::string keyFilePath;
 
+// create file. Store random key in it.
 void makeNewTopLevelKeyFile();
 
-Connection* identifyFromFrame(QWebFrame* frame);
-//identify a connection from its DOM frame element.
+// Identify a connection from its DOM frame element.
+// Return: nullptr if not found.
+Connection *identifyFromFrame(QWebFrame *frame);
 
-Container* requestContainerFromKey(
-    std::string key, std::string clientName, uint64_t pid, Connection* c
-);
+// Returns nullptr if request denied.
+Container *requestContainerFromKey(std::string key, std::string clientName,
+                                   uint64_t pid, Connection *c);
