d1c6870599f87ba989461a81d71d14fc49b2e528
Michael dinh <mdin16@studdent.monash.edu>
2021-08-07 23:40:50 +1000

    added common test

---
+++ server/gtest/src/common_test.cpp
@@ -0,0 +1,19 @@
+
+#include "common_test.h"
+
+
+
+TEST(Instruction_decoder_feed, ifstatement)
+{
+
+}
+
+TEST(Instruction_decoder_feed, elsestatement)
+{
+    
+}
+
+TEST(Instruction_decoder_feed, sameFunction)
+{
+    
+}
---
+++ server/gtest/src/common_test.h
@@ -0,0 +1,7 @@
+
+#ifndef _COMMON_TEST_H_
+#define _COMMON_TEST_H_
+
+#include "src/common.h"
+#include "gtest/gtest.h"
+#endif
