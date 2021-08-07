1ad6db5fa1225c0b43d497b5ce84ebf9132842d5
Michael dinh <mdin16@studdent.monash.edu>
2021-08-07 23:53:54 +1000

    added sanitation test

--- server/gtest/src/common_test.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef _COMMON_TEST_H_
-#define _COMMON_TEST_H_
-
-#include "src/common.h"
-#include "gtest/gtest.h"
-#endif
---
+++ server/gtest/src/sanitation.h
@@ -0,0 +1,6 @@
+#ifndef _COMMON_TEST_H_
+#define _COMMON_TEST_H_
+
+#include "../src/sanitation.h"
+#include "gtest/gtest.h"
+#endif
